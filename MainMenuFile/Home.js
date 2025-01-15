0957ec9fbb4235a5501667c3d659a8187d9f4e7b
Rias Gremory <153193656+RiasGremoryHSDXD@users.noreply.github.com>
2025-01-15 16:28:04 +0800

    Added modal form
    if the user clicked the Add entry button, the modal form will pop up and ask some details

--- MainMenuFile/Home.js
+++ MainMenuFile/Home.js
@@ -1,6 +1,15 @@
 let add_entry_button = document.getElementById("add_entry")
+let get_modal_container = document.getElementById("modal_container");
+let get_cancel_button = document.getElementById("cancel_button");
 
 add_entry_button.addEventListener('click', () =>
 {
-    console.log("Add entry is clicked!")  
+    get_modal_container.style.display = "block";
+    add_entry_button.style.display = "none";
 });
+
+get_cancel_button.addEventListener('click', () =>
+{
+    get_modal_container.style.display = "none";
+    add_entry_button.style.display = "block";
+})
