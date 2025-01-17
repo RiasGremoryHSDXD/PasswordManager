1082a7336c42f6ae86abeaf442e326b0282c08d0
Rias Gremory <153193656+RiasGremoryHSDXD@users.noreply.github.com>
2025-01-17 22:49:49 +0800

    The Add entry button enhancing the  validation

--- MainMenuFile/Home.js
+++ MainMenuFile/Home.js
@@ -7,6 +7,9 @@ let get_username = document.getElementById("save_username");
 let get_password = document.getElementById("save_password");
 let get_note = document.getElementById("save_note")
 let get_link = document.getElementById("save_link");
+let get_site_error = document.getElementById("site_error")
+let get_username_error = document.getElementById('username_error')
+let get_password_error = document.getElementById('password_error')
 
 
 add_entry_button.addEventListener('click', () =>
@@ -25,6 +28,52 @@ get_cancel_button.addEventListener('click', (event) =>
 get_add_button.addEventListener('click', (event) => 
 {
     event.preventDefault();
+    
+    let valid = true;
+
+    if(get_site_name.value.trim() === "")
+    {
+        get_site_name.style.border = "solid 2px red" 
+        get_site_error.style.display = "block"
+        valid = false;
+    }
+
+    if(get_username.value.trim() === "")
+    {
+        get_username.style.border = "solid 2px red"
+        get_username_error.style.display = "block"
+        valid = false;
+    }
+
+    if(get_password.value.trim() === "")
+    {
+        get_password.style.border = "solid 2px red"
+        get_password_error.style.display = "block"
+        valid = false;
+    }
+
+    get_site_name.addEventListener('input', () => 
+    {
+            get_site_name.style.border = "solid 1px #ced4da"
+            get_site_error.style.display = "none"
+    })
+
+    get_username.addEventListener('input', () =>
+    {
+        get_username.style.border = "solid 1px #ced4da"
+        get_username_error.style.display = "none";
+    })
+
+    get_password.addEventListener('input', () =>
+    {
+        get_password.style.border = "solid 1px #ced4da"
+        get_password_error.style.display = "none"
+    })
+
+    if(!valid)
+    {
+        return
+    }
 
     let xhr = new XMLHttpRequest()
     xhr.open("POST", "Home.php", true)
