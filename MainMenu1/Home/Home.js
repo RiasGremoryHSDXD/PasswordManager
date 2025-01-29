58fd9b53ecd80e083bc115cc62326f8e0d028dee
Rias Gremory <153193656+RiasGremoryHSDXD@users.noreply.github.com>
2025-01-29 16:28:27 +0800

    Added a modal container for if the user press the add entry button

--- MainMenu1/Home/Home.js
+++ MainMenu1/Home/Home.js
@@ -1,1 +1,75 @@
+let get_entry_button = document.getElementById("entry_button")
+let get_modal_container = document.getElementById("modal_container")
+let get_cancel_button = document.getElementById("cancel_button")
+let get_submit_button = document.getElementById("add_button")
+let get_site_name = document.getElementById("save_site_name");
+let get_username = document.getElementById("save_username");
+let get_password = document.getElementById("save_password");
+let get_note = document.getElementById("save_note")
+let get_link = document.getElementById("save_link");
+let get_site_error = document.getElementById("site_error")
+let get_username_error = document.getElementById('username_error')
+let get_password_error = document.getElementById('password_error')
+let get_close_success_modal_button = document.getElementById('close_success_modal')
+let get_success_modal = document.getElementById('success_modal')
 
+get_entry_button.addEventListener('click', () => 
+{
+    get_modal_container.style.display = "block"
+})
+
+get_cancel_button.addEventListener('click', (event) => 
+{
+    event.preventDefault()
+    get_modal_container.style.display = "none"
+})
+
+get_submit_button.addEventListener('click', (event) =>
+{
+    event.preventDefault()
+    let valid = true;
+
+    if(get_site_name.value.trim() === "")
+    {
+        get_site_name.style.border = "solid 2px red" 
+        get_site_error.style.display = "block"
+        valid = false;
+    }
+
+    if(get_username.value.trim() === "")
+    {
+        get_username.style.border = "solid 2px red"
+        get_username_error.style.display = "block"
+        valid = false;
+    }
+
+    if(get_password.value.trim() === "")
+    {
+        get_password.style.border = "solid 2px red"
+        get_password_error.style.display = "block"
+        valid = false;
+    }
+
+    get_site_name.addEventListener('input', () => 
+    {
+            get_site_name.style.border = "solid 1px #ced4da"
+            get_site_error.style.display = "none"
+    })
+
+    get_username.addEventListener('input', () =>
+    {
+        get_username.style.border = "solid 1px #ced4da"
+        get_username_error.style.display = "none";
+    })
+
+    get_password.addEventListener('input', () =>
+    {
+        get_password.style.border = "solid 1px #ced4da"
+        get_password_error.style.display = "none"
+    })
+
+    if(!valid)
+    {
+        return
+    }
+});    
--- MainMenu1/Home/Home.php
+++ MainMenu1/Home/Home.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home</title>
     <link rel="stylesheet" href="./../NavigationBar/NavigationBar.css">
+    <link rel="stylesheet" href="./../button.css">
     <link rel="stylesheet" href="Home.css">
 </head>
 
@@ -13,8 +14,34 @@
     <?php
     include('./../NavigationBar/NavigationBar.php')
     ?>
-    <div class="home_container">
+    <div class="home_container"> 
+
+        <div class="add_entry_container">
+            <button id="entry_button" class="entry_button"> + Add Entry</button>
+        </div>
+
+        <div id="modal_container" class="modal_container">
+            <form class="modal">
+                <input type="text" id="save_site_name" class="text_field_input" placeholder="Site">
+                <label id="site_error" style="display: none; color: red;">Site name is required</label>
+
+                <input type="text" id="save_username" class="text_field_input" placeholder="Username">
+                <label id="username_error" style="display: none; color: red;">Username is required</label>
+
+                <input type="text" id="save_password" class="text_field_input" placeholder="Password">
+                <label id="password_error" style="display: none; color: red;">Password is required</label>
+
+                <input type="text" id="save_note" class="text_field_input" placeholder="Note (Optional)">
+                <input type="text" id="save_link" class="text_field_input" placeholder="Link (Optional)">
+                <div class="btn_container">
+                    <button id="cancel_button" class="btn_attributes">Cancel</button>
+                    <button id="add_button" class="btn_attributes">Add</button>
+                </div>
+            </form>
+        </div>
+
     </div>
+
 </body>
     <script src="Home.js"></script>
 </html>
